66f30e85ef3d870050c3cb6eac434fdd443badcb
karamba34 <32238314+karamba34@users.noreply.github.com>
2020-07-18 05:43:20 -0700

    test

---
+++ app/src/main/java/com/example/kulearmatnie/TestingClass.java
@@ -0,0 +1,15 @@
+package com.example.kulearmatnie;
+
+class TestingClass {
+
+    String Test;
+    public TestingClass(String test) {
+        Test = test;
+    }
+
+
+
+    public String getTest() {
+        return Test;
+    }
+}
